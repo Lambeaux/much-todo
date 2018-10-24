2ad7fdee311e1fd2349651343e6db753ecb29e12
Lombardi <steve.lombardi@connexta.com>
2018-10-24 01:05:04 -0700

    Added filtering, basic styles, and fixed an indexing bug

--- vanilla/src/index.js
+++ vanilla/src/index.js
@@ -3,15 +3,29 @@ import "./index.css";
 // TODO - possibly try to separate the draw() calls from this model object
 // Look into calls to .bind() for a higher order function
 const TodoList = (function() {
+  var index = 0;
+  const filters = {
+    NONE: item => true,
+    COMPLETE: item => item.isDone === true,
+    INCOMPLETE: item => item.isDone === false
+  };
   const state = {
     itemCount: 0,
     itemCountIncompleteOnly: 0,
+    filter: "NONE",
     items: []
   };
   return {
+    getFilter() {
+      return filters[state.filter];
+    },
+    setFilter(filterName) {
+      state.filter = filterName;
+      draw();
+    },
     defaultItem() {
       return {
-        id: "todo" + state.itemCount,
+        id: "todo" + ++index,
         text: undefined,
         isDone: false,
         isEditing: false
@@ -60,6 +74,10 @@ const TodoList = (function() {
     endEdit(id, val) {
       const index = state.items.findIndex(item => item.id === id);
       const item = state.items[index];
+      // Revisit if this check is even necessary
+      if (item === undefined) {
+        return;
+      }
       item.text = val;
       state.items.forEach(item => (item.isEditing = false));
       draw();
@@ -104,11 +122,17 @@ function draw() {
     }
 
     const delBtnEl = document.createElement("BUTTON");
+    delBtnEl.innerText = "X";
     delBtnEl.addEventListener("click", () => TodoList.remove(item.id));
 
-    listEl.appendChild(checkEl);
-    listEl.appendChild(textEl);
-    listEl.appendChild(delBtnEl);
+    const wrapperDivEl = document.createElement("DIV");
+    wrapperDivEl.appendChild(checkEl);
+    wrapperDivEl.appendChild(textEl);
+    wrapperDivEl.appendChild(delBtnEl);
+
+    wrapperDivEl.setAttribute("class", "todo-item");
+
+    listEl.appendChild(wrapperDivEl);
 
     return listEl;
   };
@@ -125,8 +149,13 @@ function draw() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
-  TodoList.getState().items.map(item => list.appendChild(itemToDomNode(item)));
+  TodoList.getState()
+    .items.filter(TodoList.getFilter())
+    .map(item => list.appendChild(itemToDomNode(item)));
 }
+
+const filterEl = document.getElementById("filter");
+filterEl.addEventListener("change", () => TodoList.setFilter(filterEl.value));
 
 document
   .getElementById("completeAll")
